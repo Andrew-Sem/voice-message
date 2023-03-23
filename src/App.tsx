a54e21ccffb27f0f1111d57e83ad6f37228e6322
Andrew-Sem <avvyayvayva@gmail.com>
2023-03-23 21:05:39 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+      <div className={"w-screen h-screen bg-slate-900 text-white flex items-center justify-center"}>
+          <h1 className={"text-5xl"}>Грёбанные голосовые</h1>
+      </div>
+  )
+}
+
+export default App
